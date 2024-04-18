de4c0cdbd0ac93a1805f36f879048fcb7e46a0e7
Ella Järvinen <eltsu.jarvinen@gmail.com>
2024-04-18 11:15:37 +0300

    koodia

--- js/script.js
+++ js/script.js
@@ -1,32 +1,35 @@
+var sayMyName = function (name) {
+    alert('My name is: '+name);
+}
 
-        var car = {
-            make: 'VW',
-            type: 'Polo',
-            color: 'blue',
-            isTurnedOn: false,
-            numberOfWheels: 4,
-            seats: [
-                'seat 1',
-                'seat 2',
-                'seat 3',
-                'seat 4'
-             ],
-             turnOn: function () {
-                 this.isTurnedOn = true;
-             },
-             fly: function () {
-                 alert('fly');
+var car = {
+     make: 'VW',
+     type: 'Polo',
+     color: 'blue',
+     isTurnedOn: false,
+     numberOfWheels: 4,
+    seats: [
+     'seat 1',
+     'seat 2',
+     'seat 3',
+     'seat 4'
+    ],
+    turnOn: function () {
+     this.isTurnedOn = true;
+    },
+     fly: function () {
+         alert('fly');
+    },
+    
+    switchCar: function (isOn) {
+     console.log('turn car '+isOn)
+     ;
+    if (isOn == true) {
+        this.isTurnedOn = true;
+    } else {
+        this.isTurnedOn = false;
+    }
+}
+};
 
-             },
-             switchCar: function (isOn) {
-                 console.log('turn car '+isOn)
-                    ;
-                 if (isOn == true) {
-                    this.isTurnedOn = true;
-                 } else {
-                     this.isTurnedOn = false;
-                 }
-             }
-        };
-
-        console.log('Moikkamoi');
+    console.log('Moikkamoi');
