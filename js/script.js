29ee8d2a3002e917ea0b265f1a7554b1afc854ad
Ella Järvinen <eltsu.jarvinen@gmail.com>
2024-04-18 13:20:22 +0300

    väriboxi

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+
+var doCoolStuff = function () {
+    var currentClassName = document.getElementById('cool').className;
+
+    // changing class name
+    if (currentClassName == 'cool') {
+        document.getElementById('cool').className = 'cool red';
+} else {
+    document.getElementById('cool').className = 'cool'
+}
+}
+
+
+
 var sayMyName = function (name) {
     alert('My name is: '+name);
 }
@@ -9,10 +23,10 @@ var car = {
      isTurnedOn: false,
      numberOfWheels: 4,
     seats: [
-     'seat 1',
-     'seat 2',
-     'seat 3',
-     'seat 4'
+         'seat 1',
+         'seat 2',
+         'seat 3',
+         'seat 4'
     ],
     turnOn: function () {
      this.isTurnedOn = true;
